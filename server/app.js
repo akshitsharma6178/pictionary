47aa9816dc219d29ef469d8cc0c813f8eca8325d
William Mao <tomcat_mao@hotmail.com>
2023-07-29 14:25:32 -0600

    backend

--- server/app.js
+++ server/app.js
@@ -1,18 +1,29 @@
-const express = require("express");
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 const cors = require("cors");
-const getLogger = require("./logger");
-const apiRouter = require("./routes/apiRouter");
 
-const { PORT } = process.env;
-const logger = getLogger("server");
 const app = express();
-
 app.use(cors());
-app.use(express.json());
-app.use("/api", apiRouter);
+const server = http.createServer(app);
+const io = socketIO(server);
 
-const server = app.listen(PORT, () => {
-  logger.log(`Server running on port ${PORT}`);
+
+const onlineUsers = new Set();
+let drawerSocketId = null;
+
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('draw', (data) => {
+    console.log('draw');
+    console.log(data);
+    socket.broadcast.emit('draw', data);
+  });
+
 });
 
-module.exports = server;
+const port = 3001;
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
